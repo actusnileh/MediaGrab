3d495bfcf1fbd492d7756c114cb232ef91c2b267
Actus Nileh <actusnileh@gmail.com>
2024-07-14 14:41:13 +0300

    Fix: sponsorblock

--- services/sponsorblock.py
+++ services/sponsorblock.py
@@ -17,8 +17,6 @@ def get_sponsor_segments(url) -> list[str]:
             if segment["category"] in ["sponsor", "selfpromo", "interaction"]:
                 start_time = segment["segment"][0]
                 end_time = segment["segment"][1]
-                formatted_segment = [
-                    f"{strftime('%H:%M:%S', gmtime(start_time))} - {strftime('%H:%M:%S', gmtime(end_time))}"
-                ]
+                formatted_segment = f"{strftime('%H:%M:%S', gmtime(start_time))} - {strftime('%H:%M:%S', gmtime(end_time))}"
                 sponsor_segments.append(formatted_segment)
     return sponsor_segments
